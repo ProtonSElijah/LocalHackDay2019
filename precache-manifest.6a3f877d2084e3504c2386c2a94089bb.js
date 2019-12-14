9d52d882e3b1b7c2bfe29437ef19eb88af84b706
ProtonSElijah <SElijah@protonmail.com>
2019-12-14 12:30:03 +0300

    Updates

---
+++ precache-manifest.6a3f877d2084e3504c2386c2a94089bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5d34d81f82c54b089949d85db1522e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afdfe1a723f3a73c62ec",
+    "url": "/static/js/2.e0be4dc4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e0be4dc4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3104532467f6ad918141",
+    "url": "/static/js/main.84b1dbe2.chunk.js"
+  },
+  {
+    "revision": "1efab3b5d6e432240f59",
+    "url": "/static/js/runtime-main.92e64baa.js"
+  }
+]);
