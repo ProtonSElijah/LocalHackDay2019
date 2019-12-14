b0eab32fd3cbd097b54ee69aa1aba89ffb00b197
ProtonSElijah <SElijah@protonmail.com>
2019-12-14 12:36:33 +0300

    Updates

---
+++ precache-manifest.62f0fe39ac06ecf8af7e8c4a2b33d64d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b36040c3f8edb82f014a7c80f6b0c3e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "afdfe1a723f3a73c62ec",
+    "url": "./static/js/2.e0be4dc4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.e0be4dc4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8db6a9a5fdebe5ecbcb6",
+    "url": "./static/js/main.b45200ed.chunk.js"
+  },
+  {
+    "revision": "1f2077f1d1df69d135c2",
+    "url": "./static/js/runtime-main.34454ab3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e57f0dba6ef846cd85cb4f530cf3a41d.js"
+  "./precache-manifest.62f0fe39ac06ecf8af7e8c4a2b33d64d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
