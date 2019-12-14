ab7b3acd366ce0765b6293b975ff1240d34565fb
ProtonSElijah <SElijah@protonmail.com>
2019-12-14 16:17:00 +0300

    Added interface

--- src/App.js
+++ src/App.js
@@ -1,16 +1,80 @@
 import React, {useEffect, useState} from 'react';
+import './ResetBrowser.css';
+import './App.css';
 
 const App = () => {
+    const [products, setProducts] = useState(null);
+    const [qrData, setQrData] = useState("");
 
-    const handleClick = () => {
-        alert("wow");
+    const getProductsList = e => {
+       /* console.log(qrData.slice(qrData.indexOf("fn"), qrData.length));
+        let numberFN = qrData.slice(qrData.indexOf("fn") + 3, qrData.indexOf("&i"));
+        let numberFD = qrData.slice(qrData.indexOf("i") + 2, qrData.indexOf("&fp"));
+        let numberFPD = qrData.slice(qrData.indexOf("fp") + 3, qrData.indexOf("&n"));
+        console.log(numberFN);
+        console.log(numberFD);
+        console.log(numberFPD);
+        fetch(`https://proverkacheka.nalog.ru:9999/v1/inns/*//*kkts*//*/fss/${numberFN}/tickets/${numberFD}?fiscalSign=${numberFPD}&sendToEmail=no`,
+           {
+                method: "GET",
+                mode: "cors",
+                credentials: "same-origin",
+                headers: {
+                    "Authorization": 'Basic '+btoa(`+79291007693:236739`),
+                    'Content-Type': 'application/json',
+                     "device-id": "",
+                    "device-os": ""
+                },
+            })
+        .then(response => response.json())
+        .then(data => console.log(data));*/
+
+        e.preventDefault();
     };
 
+    useEffect(() => {
+
+    }, []);
+
   return (
-      <div>
-        <button onClick={handleClick}>Тыкни</button>
+      <div className="app">
+          <div className="header">
+            <p>Eation</p>
+          </div>
+          <div className="list">
+           <Product name="Лапша" quantity="2" />
+           <Product name="Лапша" quantity="2" />
+           <Product name="Лапша" quantity="2" />
+           <Product name="Лапша" quantity="2" />
+           <Product name="Лапша" quantity="2" />
+           <Product name="Лапша" quantity="2" />
+           <Product name="Лапша" quantity="2" />
+           <Product name="Лапша" quantity="2" />
+           <Product name="Лапша" quantity="2" />
+           <Product name="Лапша" quantity="2" />
+           <Product name="Лапша" quantity="2" />
+           <Product name="Лапша" quantity="2" />
+           <Product name="Лапша" quantity="2" />
+          </div>
+          <div className="qrblock">
+              <input type="text" value={qrData}
+               placeholder="Вставь QR с чека!"
+                onChange={(e) =>
+                    setQrData(e.currentTarget.value)}>
+            </input>
+              <button onClick={getProductsList}>Добавить покупки</button>
+          </div>
       </div>
   );
+}
+
+const Product = ({name, quantity}) => {
+    return(
+        <div className="product">
+            <p>{name}</p>
+            <p>{quantity}</p>
+        </div>
+    );
 }
 
 export default App;
